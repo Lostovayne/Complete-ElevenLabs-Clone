4d984c4c0f60446465c5e72c1c93b0a041212ce3
Deus lo Vult <92962731+Lostovayne@users.noreply.github.com>
2026-04-08 00:25:16 -0400

    chore: update Prisma and other package dependencies to latest versions for improved stability style: format code for consistency in layout and dashboard components

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -3,12 +3,12 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
 
 const DashboardPage = () => {
-  return (
-    <div className="flex flex-col justify-center items-center min-h-screen ">
-      <SidebarTrigger className="mb-4" />
-      owo
-    </div>
-  );
+	return (
+		<div className="flex flex-col justify-center items-center min-h-screen ">
+			<SidebarTrigger className="mb-4" />
+			owo
+		</div>
+	);
 };
 
 export default DashboardPage;
